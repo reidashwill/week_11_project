44382c025aa09f8af62cfc39d5cbfe75ec51a988
Reid Ashwill <reidashwill@gmail.com>
2020-05-29 14:54:41 -0700

    create validations and change seed params to reseed the db with objects that pass validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates :content_body, presence: true
+  validates :rating, presence: true
+  validates :content_body, length: { minimum: 50 }
+  validates :content_body, length: { maximum: 250 }
+  validates :rating, numericality: { greater_than: 0}
+  validates :rating, numericality: { less_than: 6}
 end
