0e106fda3fbecda387ad25c2d4bb9542e25cc37a
Reid Ashwill <reidashwill@gmail.com>
2020-05-29 15:31:34 -0700

    troubleshooting making the 'most reviewed item' to render

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Product.destroy_all
   product = Product.create!(name: Faker::Food.ingredient, cost: Faker::Number.number(digits: 2), country_of_origin: Faker::Address.country)
 
   10.times do |review|
-    Review.create!(author: Faker::Name.first_name, content_body: Faker::Lorem.paragraph_by_chars(number: 75, supplemental: false), rating: Faker::Number.between(from: 1, to: 5), product_id: product.id )
+    Review.create!(author: Faker::TvShows::StarTrek.character, content_body: Faker::Lorem.paragraph_by_chars(number: 75, supplemental: false), rating: Faker::Number.between(from: 1, to: 5), product_id: product.id )
   end
 end
 p "Created #{Product.count} products and #{Review.count} reviews"
