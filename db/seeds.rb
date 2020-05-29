be4493b514c95fac48401336478e620212fbdd98
Reid Ashwill <reidashwill@gmail.com>
2020-05-29 10:12:53 -0700

    successfully seed the database with 50 products.  rendering as expected on products#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Food.ingredient, cost: Faker::Number.number(digits: 2), country_of_origin: Faker::Address.country)
+end
+
+p "Created #{Product.count} products"
