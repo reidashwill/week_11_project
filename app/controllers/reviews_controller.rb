23ffcc2f41b263c099f2608876fa99d2fb22cbf7
Reid Ashwill <reidashwill@gmail.com>
2020-05-29 10:34:06 -0700

    start adding crud functionality to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,29 +1,33 @@
 class ReviewsController < ApplicationController
-def index
+  
+  def new
+    @review = Review.new
+    render :new
+  end
 
+  def create
+    @review = Review.new(review_params)
+    @product = Product.find(@review.product_id)
+    if @review.save
+      redirect_to product_path(@product)
+    else 
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
-
-def new
-
-end
-
-def create
-
-end
-
-def edit
-
-end
-
-def show
-
-end
-
-def update
-
-end
-
-def destroy
-
-end
-app/views/views
