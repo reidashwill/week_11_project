691a604302fc0d0a54933a9ad0c88f32f8230524
Reid Ashwill <reidashwill@gmail.com>
2020-05-29 08:29:26 -0700

    add third migration solidifying the foreign key in the rewiew class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_151704) do
+ActiveRecord::Schema.define(version: 2020_05_29_152750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_05_29_151704) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reviews", "products"
 end
