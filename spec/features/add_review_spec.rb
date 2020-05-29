c31ce3a48f935c48ab93cadf82b05939b2ec06f7
Reid Ashwill <reidashwill@gmail.com>
2020-05-29 16:18:43 -0700

    successfully pass add review test.  running out of time for remaining tests before 5.  going to work on README next to be able to submit.

---
+++ spec/features/add_review_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "the add product proccess" do
+  it "adds a new product" do
+    Product.create!(name: "Sharks", cost: "5", country_of_origin: "China")
+    visit products_path
+    click_link 'Sharks'
+    click_link 'Add a review'
+    fill_in 'Author', :with => 'godfrey'
+    fill_in 'Content body', :with => 'dakslfjdladsjfljasdflkjdaslkfjlasdkjflksadjfkljdsalfkjasdalsdkjf'
+    fill_in 'Rating', :with => '5'
+    click_on 'Create Review'
+    click_link 'Sharks'
+    expect(page).to have_content 'godfrey'
+  end
+end
