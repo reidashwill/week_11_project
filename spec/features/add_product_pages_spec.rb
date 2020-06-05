4e6cc3262bfe4ea721275b2d2dcef35ddec4fa89
Reid Ashwill <reidashwill@gmail.com>
2020-06-05 14:03:04 -0700

    add tests to cover everything in the products controller.  the only untested peices of code remaining are from the rails helper which i will leave alone.  current coverage is at 99.53%

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -17,4 +17,20 @@ describe "the add product proccess" do
     click_on 'Create Product'
     expect(page).to have_content 'Test'
   end
+  it "shows an error message when params are not validated" do
+    admin = User.create!(email: "admin@test.com", password: "password", password_confirmation: "password", admin: true) 
+    visit 'signin'
+    fill_in 'email', :with => 'admin@test.com'
+    fill_in 'password', :with => 'password'
+    within("form") do
+      click_on 'Sign in'
+    end
+    visit products_path
+    click_link 'Create a product'
+    fill_in 'Name', :with => ''
+    fill_in 'Cost', :with => ''
+    fill_in 'Country of origin', :with => ''
+    click_on 'Create Product'
+    expect(page).to have_content 'Please fix these errors'
+  end
 end
--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -19,4 +19,22 @@ describe "the edit a product proccess" do
     click_on 'Update Product'
     expect(page).to have_content 'Godfrey'
   end
+  it "shows error message when params are not validated" do
+    Product.create!(name: "Sharks", cost: "5", country_of_origin: "China")
+    admin = User.create!(email: "admin@test.com", password: "password", password_confirmation: "password", admin: true) 
+    visit 'signin'
+    fill_in 'email', :with => 'admin@test.com'
+    fill_in 'password', :with => 'password'
+    within("form") do
+      click_on 'Sign in'
+    end
+    visit products_path
+    click_link 'Sharks'
+    click_link 'edit this product'
+    fill_in 'Name', :with => 'godfrey'
+    fill_in 'Cost', :with => '3'
+    fill_in 'Country of origin', :with => ''
+    click_on 'Update Product'
+    expect(page).to have_content 'Please fix these errors'
+  end
 end
