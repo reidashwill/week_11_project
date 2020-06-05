e6f4d74b46c985d999aa6a7583d246c40b6a93e8
Reid Ashwill <reidashwill@gmail.com>
2020-06-05 08:32:31 -0700

    update application controller to utilize the current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  # Add additional helper method for admin authentication here?
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
 end
