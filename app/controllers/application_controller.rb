8a2d422632901662be279c99b738666d71773495
Reid Ashwill <reidashwill@gmail.com>
2020-06-05 08:38:33 -0700

    add authorize method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find(session[:user_id])
     end
   end
+
+  def authorize
+    if !current_user
+      flash[:alert] = "You aren't authorized to visit that page."
+      redirect_to '/'
+    end
+  end
+  
 end
