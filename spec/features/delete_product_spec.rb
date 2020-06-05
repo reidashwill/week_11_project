528f4ef902fec43816d24e6c289b8ca903489707
Reid Ashwill <reidashwill@gmail.com>
2020-06-05 11:35:57 -0700

    added admin signin to other tests needing admin access.  all passing as expected.  current test coverage at 91%

--- spec/features/delete_product_spec.rb
+++ spec/features/delete_product_spec.rb
@@ -1,11 +1,18 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "the edit a product proccess" do
-#   it "edits a product do" do
-#     Product.create!(name: "Sharks", cost: "5", country_of_origin: "China")
-#     visit products_path
-#     click_link 'Sharks'
-#     click_on 'Delete this product'
-#     expect(page).to have_no_content 'Sharks'
-#   end
-# end
+describe "the edit a product proccess" do
+  it "edits a product do" do
+    Product.create!(name: "Sharks", cost: "5", country_of_origin: "China")
+    admin = User.create!(email: "admin@test.com", password: "password", password_confirmation: "password", admin: true) 
+    visit 'signin'
+    fill_in 'email', :with => 'admin@test.com'
+    fill_in 'password', :with => 'password'
+    within("form") do
+      click_on 'Sign in'
+    end
+    visit products_path
+    click_link 'Sharks'
+    click_on 'Delete this product'
+    expect(page).to have_no_content 'Sharks'
+  end
+end
--- spec/features/edit_project_pages_spec.rb
+++ spec/features/edit_project_pages_spec.rb
@@ -1,15 +1,22 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe "the edit a product proccess" do
-#   it "edits a product do" do
-#     Product.create!(name: "Sharks", cost: "5", country_of_origin: "China")
-#     visit products_path
-#     click_link 'Sharks'
-#     click_link 'edit this product'
-#     fill_in 'Name', :with => 'godfrey'
-#     fill_in 'Cost', :with => '3'
-#     fill_in 'Country of origin', :with => 'merica'
-#     click_on 'Update Product'
-#     expect(page).to have_content 'Godfrey'
-#   end
-# end
+describe "the edit a product proccess" do
+  it "edits a product do" do
+    Product.create!(name: "Sharks", cost: "5", country_of_origin: "China")
+    admin = User.create!(email: "admin@test.com", password: "password", password_confirmation: "password", admin: true) 
+    visit 'signin'
+    fill_in 'email', :with => 'admin@test.com'
+    fill_in 'password', :with => 'password'
+    within("form") do
+      click_on 'Sign in'
+    end
+    visit products_path
+    click_link 'Sharks'
+    click_link 'edit this product'
+    fill_in 'Name', :with => 'godfrey'
+    fill_in 'Cost', :with => '3'
+    fill_in 'Country of origin', :with => 'merica'
+    click_on 'Update Product'
+    expect(page).to have_content 'Godfrey'
+  end
+end
