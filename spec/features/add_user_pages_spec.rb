a81db4d13594b6556d4fc84763b43ff920e95634
Reid Ashwill <reidashwill@gmail.com>
2020-06-05 11:05:20 -0700

    troubleshoot signing in an admin for unit testing.  was not expecting it to be this tricky.

--- spec/features/add_user_pages_spec.rb
+++ spec/features/add_user_pages_spec.rb
@@ -10,18 +10,18 @@ describe "the add user proccess" do
     click_on 'Sign Up'
     expect(page).to have_content "You've successfully signed up"
   end
+
   it "shows error when signup does not work" do
     visit 'signup'
-    
     fill_in 'user[email]', :with => 'testing@test.com'
     fill_in 'user[password]', :with => 'passwod'
     fill_in 'user[password_confirmation]', :with => 'password'
     click_on 'Sign Up'
     expect(page).to have_content "There was a problem signing up"
   end
+
   it "shows message on log out" do
     visit 'signup'
-    
     fill_in 'user[email]', :with => 'testing@test.com'
     fill_in 'user[password]', :with => 'password'
     fill_in 'user[password_confirmation]', :with => 'password'
