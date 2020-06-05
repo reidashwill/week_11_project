cc19c125d36dad37bf8035b20aaeccd542a67882
Reid Ashwill <reidashwill@gmail.com>
2020-06-05 13:10:36 -0700

    add passing test to verify that the review detail page renders when clicked on.

--- spec/features/add_review_spec.rb
+++ spec/features/add_review_spec.rb
@@ -33,4 +33,22 @@ describe "the add product proccess" do
     click_on 'Create Review'
     expect(page).to have_content 'Content body is too short'
   end
+  it "shows detail page for review when clicked on" do
+    Product.create!(name: "Sharks", cost: "5", country_of_origin: "China")
+    visit 'signup'
+    fill_in 'user[email]', :with => 'testing@test.com'
+    fill_in 'user[password]', :with => 'password'
+    fill_in 'user[password_confirmation]', :with => 'password'
+    click_on 'Sign Up'
+    visit products_path
+    click_link 'Sharks'
+    click_link 'Add a review'
+    fill_in 'Author', :with => 'godfrey'
+    fill_in 'Content body', :with => 'dakslfjdladsgdsgsfasfagfdfdsgsfgsgsdgfdsdfgfsgfdasfasdfasfasdfsdf'
+    fill_in 'Rating', :with => '5'
+    click_on 'Create Review'
+    click_on 'godfrey'
+    expect(page).to have_content 'godfrey'
+  end
+
 end
