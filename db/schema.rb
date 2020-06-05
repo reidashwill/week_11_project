32c1ef1035d4562323f789126eb45bdab519492c
Reid Ashwill <reidashwill@gmail.com>
2020-06-05 08:11:20 -0700

    add users table to db.  migration initailly failed while also adding admin column.  going to try a second migration that only adds the admin column to the table next.

--- db/schema.rb
+++ db/schema.rb
@@ -5,8 +5,14 @@
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
-ActiveRecord::Schema.define(version: 2020_05_29_152750) do
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_06_05_150932) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
@@ -24,6 +30,12 @@ ActiveRecord::Schema.define(version: 2020_05_29_152750) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
   end
 
   add_foreign_key "reviews", "products"
