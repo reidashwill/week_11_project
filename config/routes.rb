c56c1041e005d1706320af22507c7583d7b09d9a
Reid Ashwill <reidashwill@gmail.com>
2020-06-05 08:18:54 -0700

    add needed user and session routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews
   end
+
+  # User Routes
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  # Session Routes
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 end
